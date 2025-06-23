56635a8bdb93e9f8545a2dffb793dee45a25ab9f
ErickDH25 <l22170682@culiacan.tecnm.mx>
2025-06-23 07:57:37 -0700

    Selección de archivos

--- Principal.java
+++ Principal.java
@@ -1,9 +1,29 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
 
 public class Principal {
     public static void main(String[] args) throws IOException {
-        String codigo = "var1 int ; var2 int ; var1 := var2 + var1 ; print var1 + var2 ;  ";
-        new Parser(codigo);
-    }
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            //----------
+        }
 
+        JFileChooser fileChooser = new JFileChooser();
+        int res = fileChooser.showOpenDialog(null);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            String rutaArchivo = fileChooser.getSelectedFile().getAbsolutePath();
+            if (!rutaArchivo.endsWith(".txt")) {
+                System.err.println("El archivo seleccionado no es un archivo de texto (.txt).");
+                return;
+            }
+            String codigo = Files.readString(Paths.get(rutaArchivo));
+            new Parser(codigo);
+        } else {
+            System.out.println("No se seleccionó ningún archivo.");
+        }
+    }
 }
